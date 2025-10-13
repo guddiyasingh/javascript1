f59fbf0e52ed62145da57c187d11e62be442aea4
Guddiya Singh <guddiyasingh1847@gmail.com>
2025-10-13 16:01:02 +0530

    add some code

--- script2.js
+++ script2.js
@@ -1,27 +1,33 @@
-function createCard(title, cName, views, monthsOld, duration,thumbnail) {
-    // Finsh this function
-    let viewStr
-    if(views<1000000){
-        viewStr = views/1000 + "k views"
-    }
+function createCard(title, cName, views, monthsOld, duration, thumbnail){
+    //Finish this function
+     let viewStr 
+     if(views<1000000){
+        viewStr = views/1000 + "k";
+     }
+     else if(views>1000000){
+        viewStr = views/1000000 + "M";
+     }
+     else{
+        viewStr = views/1000 + "k";
+     }
+    let html =`<div class="card">
+      <div class = "image">
+      <img src = "${thumbnail}" alt = "">
+       <div class = "capsule">${duration}</div>
+       </div>
+       <div class ="text">
+      <h1>${title}</h1>
+     <p>${cName} .${viewStr} views .${monthsOld} months ago</p>
+     </div>
+     </div>`
+      
 
-    else if(views>1000000){
-        viewStr = views/1000000 + "M views";
-    }
-
-    else{
-        viewStr = views/1000 + "k views"
-    }
-
-    let html =`<div class = "card>
-    <div class = "image">
-    <img src = "${thumbnail}" alt="">
-    div class = "image"
-    <div class = "capsule"></div>
-    </div>
-    <div class = "text">
-    <h1>${title}</h1>
-    <p>${cName}.${viewStr} views . 2 months ago </p>
-     </div> 
-     </div>     `
+document.querySelector(".container").innerHTML =  document.querySelector(".container").innerHTML + html
 }
+
+
+createCard("Introduction to Backed | sigma Web Dev video #2 ", "CodeWithHarry",
+   560000,7,"31:22","https://i.ytimg.com/vi/mCx5aSEK8YE/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLD0nnsOR8c-HQ0AYnnFDHnrACrK0A")
+
+
+
