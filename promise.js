afc99ba0c894ce8b02d76115a439a02defc12d7e
Guddiya Singh <guddiyasingh1847@gmail.com>
2025-10-23 16:27:54 +0530

    add some code

--- promise.js
+++ promise.js
@@ -1,4 +1,4 @@
-console.log('This is promises');
+// console.log('This is promises');
 
 
 // let prom1 = new Promise((resolve, reject) =>{
@@ -13,24 +13,48 @@ console.log('This is promises');
 // })
 
 
-let prom1 = new Promise((resolve, reject) =>{
+// let prom1 = new Promise((resolve, reject) =>{
 
-let a = Math.random();
-if(a < 0.5){
-    reject("No random number was not supporting you")
+// let a = Math.random();
+// if(a < 0.5){
+//     reject("No random number was not supporting you")
+// }
+// else{
+
+//     setTimeout(() =>{
+//                 console.log("Yes I am done")
+//                 resolve("Harry")
+//             }, 3000);
+// }
+
+// })
+
+// prom1.then((a) =>{
+//     console.log(a)
+// }).catch((err) =>{
+//    console.log(err) 
+// })
+
+
+
+
+
+
+function walkDog(){
+    setTimeout(()=>{
+        console.log("you walked the dog ");
+    },1500)
 }
-else{
 
+function cleanKitchen(){
     setTimeout(() =>{
-                console.log("Yes I am done")
-                resolve("Harry")
-            }, 3000);
+        console.log("you cleande the kitchen")
+    },2500)
 }
 
-})
-
-prom1.then((a) =>{
-    console.log(a)
-}).catch((err) =>{
-   console.log(err) 
-})
+function takeOutTrash(){
+    setTimeout(() =>{
+        console.log("you took out the trash");
+        useCallback();
+    },500)
+}
